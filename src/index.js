54f06f11271eea587dceee23cf36b6f1aa6561f5
AlondraHackerBae <khrissy921@gmail.com>
2025-06-25 18:12:01 -0500

    added age requirement to site

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+age = prompt("Hi! You must be 21^ to enter. Please enter your age.");
+if (age < 21) {
+  alert("Sorry, you are not old enough.");
+} else {
+  alert("Cheers! Welcome in!");
+}
+
 function darkMode() {
   let body = document.querySelector("body");
   body.classList.toggle("dark");
